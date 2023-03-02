7a7933eb165e7b9de1fd6636ed60c01fe4e9416a
Alex Damascena <apfd@cin.ufpe.br>
2023-03-01 22:51:37 -0300

    feature(agent): create agent and q algorithm

--- GameController.py
+++ GameController.py
@@ -1,24 +1,28 @@
 from connection import connect, get_state_reward
 from Matrix import Matrix
+from QAgent import QAgent
 
 class GameController:
 
     def __init__(self):
         self.__matrix_loader = Matrix()
+        self.__agent = QAgent()
         self.__start_connection_with_qlearning_app()
 
     
     def __start_connection_with_qlearning_app(self):
         self.__socket = connect(2037)
-        self.__
 
     def start_controller(self):
         matrix = self.__matrix_loader.get_matrix()
-        
+        last_state = '000000'
+
         while True:
-            print("interação com o rolê")
-
-
+            action = self.__agent.get_action(matrix, last_state)
+            state, reward = get_state_reward(self.__socket, action)
+            matrix = self.__agent.q_algorithm(matrix, state, last_state, action, reward)
+            last_state = state
+            self.__matrix_loader.update_matrix(matrix)
 
 if __name__ == "__main__":
     gameController = GameController()
---
+++ QAgent.py
@@ -0,0 +1,48 @@
+from random import randint
+from State import State
+
+class QAgent:
+
+    ACTIONS = ["left", "right", "jump"]
+
+    def __init__(self):
+        print("10")
+
+
+    def get_action(self, matrix: list[State], state: str) -> str:
+        exploration = randint(0, 12)
+        agent_state = int(state, 2)
+
+        if exploration > 8:
+            agent_state_value = matrix[agent_state]
+            action_value = max(agent_state_value.left, agent_state_value.right, agent_state_value.jump)
+
+            if action_value == agent_state_value.left:
+                return "left"
+            elif action_value == agent_state_value.right:
+                return "right"
+            return "jump"
+
+        return QAgent.ACTIONS[randint(0, 2)]
+
+    def q_algorithm(self, matrix: list[State], state: str, last_state: str, action: str, reward: int) -> list[State]:
+        q_max = 0
+        agent_state = int(state, 2)
+        agent_last_state = int(last_state, 2)
+
+        line_state = matrix[agent_state]
+        q_max = max(line_state.left, line_state.right, line_state.jump)
+
+
+        if action == "jump":
+            matrix[agent_last_state].jump += 0.6 * ((reward + 0.42 * q_max) - matrix[agent_last_state].jump)
+        elif action == "left":
+            matrix[agent_last_state].left += 0.6 * ((reward + 0.42 * q_max) - matrix[agent_last_state].left)
+        else:
+            matrix[agent_last_state].right += 0.6 * ((reward + 0.42 * q_max) - matrix[agent_last_state].right)
+
+        return matrix
+
+
+
+
